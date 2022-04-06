3ca95c59f0e4d525b7ccb44c8ba2cb070a559ba5
taxeric <1248389474@qq.com>
2022-04-06 21:54:37 +0800

    调试UI

--- app/src/main/java/com/lanier/remoteview/glance/TestGlanceWidget.kt
+++ app/src/main/java/com/lanier/remoteview/glance/TestGlanceWidget.kt
@@ -1,21 +1,20 @@
 package com.lanier.remoteview.glance
 
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.glance.GlanceModifier
+import androidx.glance.Image
+import androidx.glance.ImageProvider
 import androidx.glance.appwidget.GlanceAppWidget
 import androidx.glance.appwidget.background
 import androidx.glance.appwidget.cornerRadius
-import androidx.glance.layout.Column
-import androidx.glance.layout.fillMaxSize
-import androidx.glance.layout.fillMaxWidth
-import androidx.glance.layout.padding
+import androidx.glance.layout.*
 import androidx.glance.text.FontWeight
 import androidx.glance.text.Text
 import androidx.glance.text.TextStyle
 import androidx.glance.unit.ColorProvider
+import com.lanier.remoteview.R
 import com.lanier.remoteview.ui.theme.Purple200
 import com.lanier.remoteview.ui.theme.Purple500
 import com.lanier.remoteview.ui.theme.White
@@ -27,17 +26,44 @@ import com.lanier.remoteview.ui.theme.White
 class TestGlanceWidget: GlanceAppWidget() {
     @Composable
     override fun Content() {
-           Column(modifier = GlanceModifier
-                .fillMaxSize()
-                .background(day = Purple200, night = Purple500)
-                .cornerRadius(10.dp)
-                .padding(8.dp)
+        Column(modifier = GlanceModifier
+            .fillMaxWidth()
+            .wrapContentHeight()
+            .background(day = Purple200, night = Purple500)
+            .cornerRadius(10.dp)
+            .padding(10.dp)
+        ) {
+            Row (
+                modifier = GlanceModifier.fillMaxWidth().fillMaxHeight(),
+                verticalAlignment = Alignment.CenterVertically
             ) {
-                 Text(
-                   text = "风追烟花雨",
-                   modifier = GlanceModifier.fillMaxWidth(),
-                   style = TextStyle(fontWeight = FontWeight.Bold, color = ColorProvider(White)),
-                  )
+                Image(
+                    modifier = GlanceModifier.width(70.dp).height(70.dp).cornerRadius(10.dp),
+                    provider = ImageProvider(R.drawable.ic_avatar),
+                    contentDescription = "avatar"
+                )
+                Spacer(modifier = GlanceModifier.width(10.dp))
+                Column (modifier = GlanceModifier.fillMaxSize()) {
+                    Text(
+                        text = "风追烟花雨 🎇",
+                        modifier = GlanceModifier.fillMaxWidth(),
+                        style = TextStyle(
+                            fontSize = 16.sp,
+                            fontWeight = FontWeight.Bold,
+                            color = ColorProvider(White)
+                        ),
+                    )
+                    Spacer(modifier = GlanceModifier.height(10.dp))
+                    Text(
+                        text = "氤氲冉作霜",
+                        modifier = GlanceModifier.fillMaxWidth(),
+                        style = TextStyle(
+                            fontWeight = FontWeight.Bold,
+                            color = ColorProvider(White)
+                        )
+                    )
                 }
+            }
+        }
     }
 }
